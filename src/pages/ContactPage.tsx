204ca0f89568164b18bae0f1c6fe29a69aaefdfa
hersariz <rizkymvp123@gmail.com>
2025-04-18 03:37:24 +0700

    Memperbaiki ikon TikTok dengan menggunakan react-icons

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,6 +1,7 @@
 import { Clock, Mail, MapPin, Phone } from 'lucide-react';
 import ContactForm from '../components/ContactForm';
 import { useLanguage } from '../contexts/LanguageContext';
+import { FaTiktok, FaInstagram } from 'react-icons/fa';
 
 const ContactPage = () => {
   const { t } = useLanguage();
@@ -100,14 +101,10 @@ const ContactPage = () => {
                 <h3 className="text-lg font-semibold text-gray-800 dark:text-white mb-4">{t('connectWithUs')}</h3>
                 <div className="flex space-x-4">
                   <a href="#" className="w-10 h-10 flex items-center justify-center rounded-full bg-indigo-100 dark:bg-indigo-900/30 text-indigo-600 dark:text-indigo-400 hover:bg-indigo-600 hover:text-white dark:hover:bg-indigo-700 transition-colors">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 24 24">
-                      <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
-                    </svg>
+                    <FaInstagram size={20} />
                   </a>
                   <a href="#" className="w-10 h-10 flex items-center justify-center rounded-full bg-indigo-100 dark:bg-indigo-900/30 text-indigo-600 dark:text-indigo-400 hover:bg-indigo-600 hover:text-white dark:hover:bg-indigo-700 transition-colors">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 24 24">
-                      <path d="M9 12.34L11.78 15.12L18.35 8.55M21.08 8C20.71 6.7 20 5.5 19.03 4.53C18.05 3.56 16.86 2.85 15.56 2.48C14.15 2.08 12.67 2.08 11.27 2.48C9.95 2.85 8.76 3.56 7.79 4.53L4.5 7.82V11C4.5 11 8.35 10.4 10.04 12.08C11.73 13.77 11 17.5 11 17.5H14.18L17.47 14.21C18.44 13.23 19.15 12.05 19.52 10.73C19.92 9.33 19.92 7.85 19.52 6.44M14.5 14.5L9.5 9.5" />
-                    </svg>
+                    <FaTiktok size={20} />
                   </a>
                 </div>
               </div>
